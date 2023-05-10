b3f2505c1068eeff1d1ec8dc3bedadb83bbf8377
a-tibi <ahmd_tibi@yahoo.com>
2023-05-10 19:59:48 +0300

    first commit

---
+++ DateTimeExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateTime_ExtensionMethod
+{
+	static class DateTimeExtension
+	{
+		public static bool IsWeekEnd(this DateTime dt)
+		{
+			return dt.DayOfWeek == DayOfWeek.Friday || dt.DayOfWeek == DayOfWeek.Saturday; 
+		}
+
+		public static bool IsWeekDay(this DateTime dt)
+		{
+			return !IsWeekEnd(dt);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateTime_ExtensionMethod
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var dt = DateTime.Now;
+			dt = dt.AddDays(2);
+			Console.WriteLine(dt.DayOfWeek);
+			Console.WriteLine($"Is week end: {dt.IsWeekEnd()}");
+			Console.WriteLine($"Is week day: {dt.IsWeekDay()}");
+			Console.ReadKey();
+		}
+	}
+}
